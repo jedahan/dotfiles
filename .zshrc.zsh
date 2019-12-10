98ce01f083d148b4c7d4cd26abe376713d00267c
Jonathan Dahan <hi@jonathan.is>
2019-12-10 15:17:04 -0500

    document sections of zshrc

--- .zshrc.zsh
+++ .zshrc.zsh
@@ -1,30 +1,20 @@
+# tmux
 icons=(                     )
 existing=($(tmux list-windows -F'#W'|paste -s -))
 available=(${icons:|existing})
 export ICON=${available[RANDOM % $#available + 1]}
 grep -q zsh <(tmux list-windows -F'#W') && tmux rename-window $ICON
-
 tmux bind-key -n C-n new-window -n $ICON
 
+# zsh options
 bindkey -e
 setopt autocd autopushd pushd_ignore_dups interactivecomments
 setopt bang_hist extended_history inc_append_history share_history hist_ignore_space hist_verify
-
 autoload -Uz select-word-style && select-word-style bash
-
 export HISTFILE=${HOME}/.zhistory HISTSIZE=100000 SAVEHIST=100000 ZSH_AUTOSUGGEST_STRATEGY=match_prev_cmd
+zstyle ":history-search-multi-word" page-size "$(( $LINES * 3 / 4 ))"
 
-(( $+commands[rg] )) && export FZF_DEFAULT_COMMAND='rg --files --follow'
-
-alias manual=$commands[man] \
- find=${commands[fd]:-$commands[find]} \
- grep=${commands[rg]:-$commands[grep]} \
- cat=${commands[bat]:-$commands[cat]} \
- sed=${commands[sd]:-$commands[sed]} \
- awk=${commands[sd]:-$commands[awk]} \
- fzf=${commands[sk]:-$commands[fzf]} \
- t='cd $(mktemp -d /tmp/$1.XXXX)'
-
+# plugins
 ZR=${XDG_CONFIG_HOME:-${HOME}/.config}/zr.zsh
 if [[ ! -s $ZR ]] || [[ ~/.zshrc -nt $ZR ]]; then
   zr \
@@ -40,13 +30,23 @@ if [[ ! -s $ZR ]] || [[ ~/.zshrc -nt $ZR ]]; then
     jedahan/help.zsh \
     jedahan/up.zsh > $ZR
 fi; source $ZR
-
 test -f /etc/zsh_command_not_found && source $_ || true
 
-zstyle ":history-search-multi-word" page-size "$(( $LINES * 3 / 2 ))"
+# default commands
+alias manual=$commands[man] \
+ find=${commands[fd]:-$commands[find]} \
+ grep=${commands[rg]:-$commands[grep]} \
+ cat=${commands[bat]:-$commands[cat]} \
+ sed=${commands[sd]:-$commands[sed]} \
+ awk=${commands[sd]:-$commands[awk]} \
+ fzf=${commands[sk]:-$commands[fzf]}
+
+# aliases
+(( $+commands[rg] )) && export FZF_DEFAULT_COMMAND='rg --files --follow'
+(( $+commands[exa] )) && alias tree='exa --tree --level=2'
 
 abbrev-alias x=exit \
- o=open \
+ o=xdg-open \
  c=lolcat \
  _=sudo \
  s=grep \
@@ -55,10 +55,11 @@ abbrev-alias x=exit \
  ls=exa \
  l='exa -s type' \
  ll='exa -lbGF --git' \
- tree='exa --tree --level=2' \
  ,='clear && l'
 
+# functions
+t() { cd $(mktemp -d /tmp/$1.XXXX) }
 config() { command git --git-dir=$HOME/.dotfiles --work-tree=$HOME/. "$@" }
 git() { [[ $PWD != $HOME ]] && { command git "$@"; return } || config "$@" }
-mpw() { MPW_ASKPASS=ssh-askpass command mpw -u "Jonathan Dahan" -t x -q "$@" | tee >(xsel -ib) >(xsel -i) 1>/dev/null }
+mpw() { . ~/.secrets && command mpw -t x "$@" | tee >(xsel -ib) >(xsel -i) 1>/dev/null; unset MPW_FULLNAME MPW_ASKPASS }
 alert() { notify-send --urgency=low -i "$([ $? = 0 ] && echo terminal || echo error)" "$(history|tail -n1|sed -e 's/^\s*[0-9]\+\s*//;s/[;&|]\s*alert$//')" }
