7edf2dc1f5ccdb64eab0dc4dfb02b24d651d909b
Jonathan Dahan <hi@jonathan.is>
2020-01-21 11:03:41 -0500

    zsh: create nicer bright control ramp

---
+++ .local/bin/backlight.zsh
@@ -0,0 +1,20 @@
+#!/bin/env zsh
+
+steps=(0% 1% 10% 20% 30% 50% 80% 100%)
+
+brightness=$(brightnessctl info -m | cut -d',' -f4)
+direction=${1:-up}
+
+index=${steps[(I)$brightness]}
+
+if [[ -z "$index" ]]; then return; fi
+
+[[ "$direction" = "up" ]] && (( index += 1 ))
+[[ "$direction" = "down" ]] && (( index -= 1 ))
+[[ "$index" -lt 1 ]] && (( index = 1 ))
+[[ "$index" -gt $#steps ]] && (( index = $#steps ))
+
+newbrightness=${steps[$index]}
+
+printf "backlight moving %s from %s to %s" "$direction" "$brightness" "$newbrightness"
+brightnessctl set "$newbrightness" >/dev/null
