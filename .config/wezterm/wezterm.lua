48414a3ee102236234c508136b79350fa25e345a
Jonathan Dahan <git@jonathan.is>
2024-03-19 12:55:42 -0400

    wezterm: use non-fancy tabs

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -2,4 +2,10 @@ local wezterm = require 'wezterm'
 local config = wezterm.config_builder()
 config.color_scheme = 'tokyonight'
 config.audible_bell = 'Disabled'
+config.font_size = 22.0
+
+config.use_fancy_tab_bar = false
+config.tab_max_width = 25
+config.tab_bar_at_bottom = true
+config.hide_tab_bar_if_only_one_tab = true
 return config
