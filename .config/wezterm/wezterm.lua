1f8df09626d8b49e0ddd1acac99062188eeb4c61
Jonathan Dahan <git@jonathan.is>
2024-04-24 12:35:02 -0400

    wezterm: add option-left and option-right to move words

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -8,4 +8,13 @@ config.use_fancy_tab_bar = false
 config.tab_max_width = 25
 config.tab_bar_at_bottom = true
 config.hide_tab_bar_if_only_one_tab = true
+
+-- https://github.com/wez/wezterm/issues/253#issuecomment-672007120
+config.keys = {
+	-- Make Option-Left equivalent to Alt-b which many line editors interpret as backward-word
+	{ key = "LeftArrow", mods = "OPT", action = wezterm.action({ SendString = "\x1bb" }) },
+	-- Make Option-Right equivalent to Alt-f; forward-word
+	{ key = "RightArrow", mods = "OPT", action = wezterm.action({ SendString = "\x1bf" }) },
+}
+
 return config
